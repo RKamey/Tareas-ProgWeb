2fd2495dfa221e57768dc3996c674c1287f9ba57
Kamey <ralonso@ucol.mx>
2023-05-06 15:31:55 -0600

    Práctica 21: Editar un documento a la colección de Mongo DB

--- practica_21/src/routes/persons.js
+++ practica_21/src/routes/persons.js
@@ -3,46 +3,53 @@ const router = express.Router(); // Creamos una instancia de express router
 const mongoose = require('mongoose'); // Inyectamos la dependencia de mongoose
 let Person = require('../models/persons'); // Inyectamos el modelo de la base de datos de personas
 
+// Ruta para el index
 router.get('/', (req, res) => {
     res.render('index');
 })
 
-// Ruta para crear una persona
+// Ruta para crear una persona (vista)
 router.get('/gente', async (req, res) => {
      const Persons = await Person.find({});
      res.render('persons', {Persons});
     });
 
+// Ruta para crear una persona (vista)
 router.get('/addPerson', (req, res) => {
     res.render('addPerson');
 });
 
+// Ruta para crear una persona (proceso)
 router.post('/addPerson', (req, res) => {
+    // Creamos un objeto de tipo persona
     const newPerson = Person({
-        nombre: req.body.nombre,
+        nombre: req.body.nombre, 
         edad: req.body.edad,
         tipoSangre: req.body.tipoSangre,
         nss: req.body.nss
     });
-
+    // Guardamos el objeto en la base de datos
     newPerson
     .save()
     .then((data) => {res.redirect('gente')})
     .catch((error) => {res.json({message:error})});
 })
 
+// Ruta para eliminar una persona (proceso)
 router.get('/deletePerson/:id', (req, res) => {
     Person.findByIdAndDelete(req.params.id)
     .then((data) => {res.redirect('/gente')})
     .catch((error) => {res.json({message:error})});
 });
 
+// Ruta para actualizar una persona (vista)
 router.get('/findById/:id', (req, res) => {
     Person.findById(req.params.id)
     .then((myPerson) => {res.render('personUpdate', {myPerson})})
     .catch((error) => {res.json({message:error})});
 });
 
+// Ruta para actualizar una persona (proceso)
 router.post('/updatePerson', (req, res) => {
     Person.findByIdAndUpdate(req.body.objId,
         {
